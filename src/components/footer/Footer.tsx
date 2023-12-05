00020987b7b86f5cd034014db32319a26b9bf130
lucasbenjaminson <lucas_benjaminson@hotmail.com>
2023-12-05 22:19:27 +0100

    Footer info added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,33 @@
-import Image from "next/image";
-import { HiMiniHeart } from "react-icons/hi2";
-import { data as footerData } from "./data";
+import Image from 'next/image';
+import { HiMiniHeart } from 'react-icons/hi2';
+import { data as footerData } from './data';
 
 const Footer = () => {
   return (
-    <footer className="p-4 py-12 md:px-16 md:pt-24 md:pb-12 bg-gradient-to-b from-cyan-700 to-cyan-950">
-      <div className="grid grid-cols-3 ">
-        <Image src="/logo.png" alt="" width={140} height={140} className="rounded" />
-        <div className="grid grid-cols-2 gap-8 sm:gap-6">
-          {footerData.map(({ label, links }) => (
-            <div key={label}>
-              <h2 className="mb-6 text-sm font-semibold uppercase">{label}</h2>
-              <ul className="space-y-2">
-                {links.map(({ label, href }) => (
-                  <li key={label} className="mb-4 text-xs md:text-sm">
-                    <a href={href} className="hover:underline">
-                      {label}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
+    <footer className="p-4 py-12 md:px-16 md:pt-24 md:pb-12 bg-gradient-to-b from-cyan-700 to-cyan-950 ">
+      <div className="gap-10 md:flex">
+        <div>
+          <Image src="/logo.png" alt="" width={140} height={140} className="rounded" />
+        </div>
+
+        <div className="flex md:justify-center flex-1 mt-10 md:mt-0">
+          <div className="grid grid-cols-3 gap-8 sm:gap-6">
+            {footerData.map(({ label, links }) => (
+              <div key={label}>
+                <h2 className="mb-6 text-sm font-semibold uppercase">{label}</h2>
+                <ul className="space-y-2">
+                  {links.map(({ label, href, locality }) => (
+                    <li key={label} className="mb-4 text-xs md:text-sm">
+                      <a href={href} className="hover:underline">
+                        {label}
+                      </a>
+                      <p>{locality}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <div className="h-[1px] bg-white opacity-10 w-full my-6 sm:mx-auto lg:my-8" />
@@ -30,10 +36,10 @@ const Footer = () => {
         target="_blank"
         className="hover:underline text-sm sm:text-center flex gap-1 items-center"
       >
-        Made with{" "}
+        Made with{' '}
         <span className="text-red-500 ">
           <HiMiniHeart />
-        </span>{" "}
+        </span>{' '}
         by Appstract © 2023
       </a>
     </footer>
--- src/components/footer/data.ts
+++ src/components/footer/data.ts
@@ -2,6 +2,7 @@ interface FooterLinkPartial {
   label: string;
   href: string;
   internal?: boolean;
+  locality?: string;
 }
 
 interface FooterLink {
@@ -11,30 +12,48 @@ interface FooterLink {
 
 export const data: FooterLink[] = [
   {
-    label: "Links",
+    label: 'Links',
     links: [
       {
-        label: "About",
-        href: "/about",
+        label: 'About',
+        href: '/about',
         internal: true,
       },
       {
-        label: "Contact",
-        href: "/contact",
+        label: 'Contact',
+        href: '/contact',
         internal: true,
       },
     ],
   },
   {
-    label: "Follow us",
+    label: 'Follow us',
     links: [
       {
-        label: "GitHub",
-        href: "",
+        label: 'GitHub',
+        href: '',
       },
       {
-        label: "Twitter",
-        href: "",
+        label: 'Twitter',
+        href: '',
+      },
+    ],
+  },
+  {
+    label: 'Kontakta oss',
+    links: [
+      {
+        label: 'Pruhultsvägen 37',
+        locality: '511 53 Kinna',
+        href: 'https://www.google.com/maps/place/M%C3%A5leri+Viskan/@57.506006,12.671134,17z/data=!3m1!4b1!4m6!3m5!1s0x46501b420e8f6d6f:0x6c921bc4eb4ac81b!8m2!3d57.5060031!4d12.6737089!16s%2Fg%2F11kr_z7sch?entry=ttu',
+      },
+      {
+        label: '0500-33 10 18',
+        href: 'tel:0500-33 10 18',
+      },
+      {
+        label: 'maleri-viskan@viskan.se',
+        href: 'mailto:maleri-viskan@viskan.se',
       },
     ],
   },
