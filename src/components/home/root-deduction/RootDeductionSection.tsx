7e5d3cf5ef57aefc0f09619dcd843dbdd8ef8eb7
Alexander Auermaa <alexander.auermaa@gmail.com>
2023-12-08 23:56:05 +0100

    Root Deduction section now responsive

--- src/components/home/root-deduction/RootDeductionSection.tsx
+++ src/components/home/root-deduction/RootDeductionSection.tsx
@@ -4,9 +4,9 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const RootDeductionSection = () => {
   return (
     <section className="bg-black text-white bg-gradient-to-br from-fuchsia-700 to-cyan-800">
-      <div className="max-w-screen-2xl px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 w-full mx-auto">
-        <div className="grid grid-cols-12 items-center justify-center mx-auto">
-          <div className="col-span-10">
+      <div className="max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 w-full mx-auto">
+        <div className="flex flex-col sm:flex-row items-center justify-between">
+          <div className="basis-3/4">
             <h1 className="max-w-4xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl whitespace-pre-line">
               Vill du använda ROT-avdrag?{"\n"}Vi hjälper dig!
             </h1>
@@ -15,10 +15,10 @@ const RootDeductionSection = () => {
               skattereduktion på 30% av arbetskostnaden, med en maximal gräns på 50 000 kronor.
             </p>
           </div>
-          <div className="col-span-2 flex justify-end">
+          <div className="ml-auto">
             <Link
-              href="/kontakt"
-              className="px-6 py-3 rounded-sm flex items-center gap-2 bg-white text-black hover:bg-black hover:text-white duration-200 transition-all"
+              href="/rot-avdrag"
+              className="px-2 py-2.5 md:px-6 md:py-3 text-sm md:text-md rounded-sm flex items-center gap-2 bg-white text-black hover:bg-black hover:text-white duration-200 transition-all"
             >
               Läs mer om ROT
               <FaArrowRightLong />
